1cebfd6df5b4fd542634e6f9c6b7fbb122ef2456
Thinkbiz HighTech <thinkbizh@gmail.com>
2026-03-14 17:18:24 +0530

    message

--- backend/services/phoneSMSService.js
+++ backend/services/phoneSMSService.js
@@ -1,8 +1,23 @@
-const axios = require("axios");
+  // const axios = require("axios");
+
+  // exports.sendPhoneSMS = async (mobile, message) => {
+  //   try {
+  //     await axios.post("http://YOUR_PHONE_IP:3000/send", {
+  //       number: mobile,
+  //       message: message,
+  //     });
+
+  //     console.log("SMS sent from phone");
+  //   } catch (error) {
+  //     console.log("Phone SMS error:", error.message);
+  //   }
+  // };
+
+  const axios = require("axios");
 
 exports.sendPhoneSMS = async (mobile, message) => {
   try {
-    await axios.post("http://YOUR_PHONE_IP:3000/send", {
+    await axios.post("http://192.168.1.10:3000/send", {
       number: mobile,
       message: message,
     });
