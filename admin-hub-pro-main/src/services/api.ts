98eed033bfadaa75c87247c005309d51b703064f
Thinkbiz HighTech <thinkbizh@gmail.com>
2026-03-25 14:54:38 +0530

    message

--- admin-hub-pro-main/src/services/api.ts
+++ admin-hub-pro-main/src/services/api.ts
@@ -1,10 +1,18 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   // baseURL: "https://captivating-achievement-production-7fbd.up.railway.app",
+//   //
+//   baseURL: "http://localhost:5000/api",
+//   // 🔥 apna URL
+// });
+
+// export default API;
+
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://captivating-achievement-production-7fbd.up.railway.app",
-  //
-  baseURL: "http://localhost:5000/api",
-  // 🔥 apna URL
+  baseURL: "https://captivating-achievement-production-7fbd.up.railway.app/api",
 });
 
 export default API;
